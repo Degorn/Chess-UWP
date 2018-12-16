da989aa86f32bb06b9fe24bf4da2a9da37dcf545
Даниил <degorn01@gmail.com>
2018-12-17 02:11:33 +0300

    Figure movement animation trigger in xaml.

--- Chess-UWP/Controls/FigureView.xaml.cs
+++ Chess-UWP/Controls/FigureView.xaml.cs
@@ -1,30 +1,12 @@
-﻿using Chess_UWP.ViewModels;
-using System;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+﻿using Windows.UI.Xaml.Controls;
 
 namespace Chess_UWP.Controls
 {
     public sealed partial class FigureView : UserControl
     {
-        public FigureViewModel FigureModel { get; set; }
-
         public FigureView()
         {
             this.InitializeComponent();
-        }
-
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            FigureModel = (DataContext as FigureViewModel);
-            FigureModel.Move += FigureModel_Move;
-        }
-
-        private void FigureModel_Move(object sender, EventArgs e)
-        {
-            MyStoryboard.Begin();
         }
     }
 }
