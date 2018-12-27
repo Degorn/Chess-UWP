4b674905ffe386dd74ca7d1a4af942347b375166
Даниил <degorn01@gmail.com>
2018-12-27 23:12:50 +0300

    Switched main view.

--- Chess-UWP/App.xaml.cs
+++ Chess-UWP/App.xaml.cs
@@ -74,7 +74,7 @@ namespace Chess_UWP
             if (e.PreviousExecutionState == ApplicationExecutionState.Running)
                 return;
 
-            DisplayRootView<BoardView>();
+            DisplayRootView<MainMenuView>();
         }
 
 
